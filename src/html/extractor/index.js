2c48d6ebdf96606175c12011d1e35f7795e6f038
Manoel Medeiros <manorfm@hotmail.com>
2021-09-07 19:06:15 -0300

    added 2.0 editor

--- src/html/extractor/index.js
+++ src/html/extractor/index.js
@@ -6,15 +6,48 @@ export default class HTMLExtractor {
     async extract(url) {
         try {
             const response = await got(url);
-            const { document } = new JSDOM(response.body).window;
+            const { document: body } = new JSDOM(response.body).window;
 
             //const content = document.querySelectorAll('div.chapter-content3 div.desc p');
-            const content = document.querySelectorAll('#chapterhidden p');
-            
-            const arr = Array.from(content.values())
-            return arr.map(p => p.innerHTML)
+            const content = body.querySelectorAll('#chapterhidden p');
+
+            const chapters = body.querySelectorAll('ul.chapter-actions li select')
+
+            // console.log(chapters.values())
+            // console.log(chapters.options)
+
+            const episode = Array.from(content.values())
+
+            return episode.map(p => p.innerHTML)
+                .filter(line => !line.startsWith('Tip:'))
+                .reduce((previous, current) => {
+                    const line = current.trim();
+                    if (line.startsWith('If you find any errors') || line.trim().startsWith('Tip:')) {
+                        return previous; // ignore lines
+                    }
+                    if (line.startsWith('Chapter')) {
+                        return { ...previous, title: this.formatChapter(line) }
+                    }
+                    if (line.startsWith('Translation') || line.startsWith('Editor') || line.startsWith('Translator')) {
+                        return this.toArray(previous, line, 'infos')
+                    }
+                    return this.toArray(previous, line, 'contents')
+                }, {})
         } catch (error) {
             console.log(error);
         }
+    }
+
+    toArray(previous, current, field) {
+        if (previous && previous[field]) {
+            previous[field].push(current)
+            return previous;
+        }
+        return { ...previous, [field]: [current] }
+    }
+
+    formatChapter(line) {
+        const index = line.indexOf(', ');
+        return index > 0 ? `Chapter: ${line.substring(index + 1)}` : line;
     }
 }
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,16 +5,16 @@ export default class Router {
     set(server) {
         const url = 'https://www.readlightnovel.org/martial-peak/chapter-'
 
-        server.get('/site',async (req, res) => {
+        server.get('/',async (req, res) => {
             const { chapter } = req.query
 
             const htmlExtractor = new HTMLExtractor()
             
-            const contents = await htmlExtractor.extract(`${url}${chapter}`)
+            const page = await htmlExtractor.extract(`${url}${chapter}`)
 
-            if (contents) {
+            if (page) {
                 res.status(200)
-                    .render('pages/index', { contents, chapter })
+                    .render('pages/index', { page, chapter })
             }
         })
     }
